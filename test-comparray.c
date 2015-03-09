bb1bbf20c75307e7109977e6baf8524453491ab6
Rob Latham <robl@mcs.anl.gov>
2015-03-09 11:15:43 -0500

    display pass/fail information

--- test-comparray.c
+++ test-comparray.c
@@ -7,6 +7,7 @@ int main(int argc, char **argv)
     comparray carray = comparray_alloc();
 
     int i, *sequential_items, *random_items, *zero_items;
+    int nr_errors=0;
     int *compare;
 
 #define NR_ITEMS 100
@@ -18,9 +19,12 @@ int main(int argc, char **argv)
     comparray_set_n(carray, 0, NR_ITEMS, sequential_items);
     compare = comparray_get_n(carray, 0, NR_ITEMS);
 
-    for (i=0; i<100; i+=6) {
-	printf("sequential_items[%d] = %d  compare[%d] = %d\n",
+    for (i=0; i<NR_ITEMS; i++) {
+	if (sequential_items[i] != compare[i]) {
+	    nr_errors++;
+	    printf("sequential_items[%d] = %d  compare[%d] = %d\n",
 		i, sequential_items[i], i, compare[i]);
+	}
     }
-
+    if (nr_errors == 0) printf(" No Errors\n");
 }
