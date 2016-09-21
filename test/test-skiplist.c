58ba428cedc71fe1bb7e54bfae1add782975dd27
Rob Latham <robl@mcs.anl.gov>
2016-09-21 16:10:27 -0500

    make skiplist test acutally test something

--- test/test-skiplist.c
+++ test/test-skiplist.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "skiplist.h"
 
 int main(int argc, char **argv)
 {
-    int i;
+    int i, *j, errs=0;
+    /* does it catch fire */
     skiplist *list = skiplist_new(4);
-    for (i=0; i<10; i++) {
-	skiplist_insert(list, &i, NULL);
+    i = 4;
+    skiplist_insert(list, &i, NULL);
+    skiplist_delete(list, &i);
+    if (skiplist_search(list, &i) != NULL)
+	errs++;
+
+    for(i=0; i<10; i++) {
+	j = malloc(sizeof(*j));
+	*j = i;
+	skiplist_insert(list, j, NULL);
     }
+    i=5;
+    skiplist_node *q = skiplist_search(list, &i);
+    if (q == NULL)
+	errs++;
 }
